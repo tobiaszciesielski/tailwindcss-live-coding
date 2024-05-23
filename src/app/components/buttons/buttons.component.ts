313862b54b2ed2a83d8890b9f9553faf2f1cc5a4
Tobiasz Ciesielski <tobiasz.ciesielski@prowly.com>
2024-05-23 11:23:51 +0200

    Live coding results

--- src/app/components/buttons/buttons.component.ts
+++ src/app/components/buttons/buttons.component.ts
@@ -1,16 +1,44 @@
 import { NgFor } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import {
   bootstrapChevronDown,
   bootstrapGithub,
 } from '@ng-icons/bootstrap-icons';
 import { NgIconComponent, provideIcons } from '@ng-icons/core';
+import { twMerge } from 'tailwind-merge';
+
+@Component({
+  selector: 'app-button',
+  standalone: true,
+  template: ` <button class="{{ mapper(1) }}">
+    <ng-content />
+  </button>`,
+  imports: [NgIconComponent, NgFor],
+})
+export class ButtonComponent {
+  @Input()
+  additionalClasses: string = '';
+  @Input()
+  loading: boolean = false;
+
+  twMerge = twMerge;
+
+  mapper(status: number): string {
+    const mappingObject = {
+      1: 'bg-amber-' + 950,
+    };
+
+    return mappingObject[1];
+  }
+
+  ['bg-amber-500']() {}
+}
 
 @Component({
   selector: 'app-buttons',
   standalone: true,
   templateUrl: './buttons.component.html',
-  imports: [NgIconComponent, NgFor],
+  imports: [NgIconComponent, NgFor, ButtonComponent],
   viewProviders: [provideIcons({ bootstrapGithub, bootstrapChevronDown })],
 })
 export class ButtonsComponent {}
--- src/app/components/layout/layout.component.ts
+++ src/app/components/layout/layout.component.ts
@@ -1,8 +1,10 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
+  imports: [NgFor],
   templateUrl: './layout.component.html',
 })
 export class LayoutComponent {}
